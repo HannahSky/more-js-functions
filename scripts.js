8e4be683551a36fe36e8e724b7434cdaa21f35a3
HannahSky <111460985+HannahSky@users.noreply.github.com>
2023-09-07 13:55:25 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,9 +5,22 @@ function addToPage(string){
 
 // let's write a function that accepts two arguments then adds them together. If the arguments aren't both numbers, concatenate that with the other argument and a space between the args, otherwise perform addition
 // TO DO
+function addTheValues(param1, param2){
+    if(isNaN(param1) || isNaN(param2)){
+        addToPage("One of the parameters is not a number, and when added they equal: " + param1 + " " + param2);
+    }
+    else{
+        addToPage("Both of the parameters are numbers and when added they equal: " + (param1 + param2));
+    }
+}
+
 
 // call the function a few times with different parameters
 // TO DO
+addTheValues(5, 5);
+addTheValues("hi", 5);
+addTheValues(5, "hi");
+addTheValues("Hannah", "Kaminsky");
 
 // let's talk about scope and see it in action
 // function with a local variable to display to the screen
@@ -140,7 +153,7 @@ function helpCalcChange(e){
 document.getElementById("calculate").addEventListener("click", helpCalcChange);
 
 // this code updates the year in the footer for the copyright on page load
-// this style of function  is an immediately invoked function expression (IIFE)
+// this style of function  is an immediately invoked function expression "IIFE"
 // it is an anonymous function that calls itself and only runs on page load once
 (function(){
 	let now = new Date();
